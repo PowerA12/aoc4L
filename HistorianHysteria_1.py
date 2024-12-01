70d610fe189ba10d740079895d1bfb286b9ff1c1
GodZen <HydraIsAgoodTool@protonmail.com>
2024-12-01 12:34:39 +0545

    1st day

---
+++ HistorianHysteria_1.py
@@ -0,0 +1,14 @@
+with open('HHinput.txt', 'r') as f:
+    left = []
+    right = []
+    s = 0
+    output = f.read().split('\n')[0:-1]
+    for i in output:
+        val = i.split('   ')
+        left.append(int(val[0]))
+        right.append(int(val[1]))
+    left = sorted(left)
+    right = sorted(right)
+    for j in range(len(left)):
+        s += abs(left[j] - right[j])
+    print(s)
---
+++ HistorianHysteria_2.py
@@ -0,0 +1,14 @@
+with open('HHinput.txt', 'r') as f:
+    left = []
+    right = []
+    s = 0
+    output = f.read().split('\n')[0:-1]
+    for i in output:
+        val = i.split('   ')
+        left.append(int(val[0]))
+        right.append(int(val[1]))
+    left = sorted(left)
+    right = sorted(right)
+    for i in left:
+        s += i * right.count(i)
+    print(s)
